0497b0298b59e2bac3fd7fc92730888b6b59e6cc
Austin Mroczek <austin@mroczek.org>
2024-11-10 20:25:25 +0000

    Revert "use jq for most config entries"
    This reverts commit c80c654169ef7e1e5829dcdac898a2ec13bed1ca.

--- solarman_mqtt/run.sh
+++ solarman_mqtt/run.sh
@@ -13,14 +13,14 @@ if [ ! -f "config.json" ]; then
 fi
 
 bashio::log.info "Pull Solarman config from HA add-on config"
-SM_USERNAME=$(bashio::config 'solarman_username'  | jq -Rsa .)
+SM_USERNAME=$(bashio::config 'solarman_username')
 SM_PASSWORD=$(bashio::config 'solarman_password')
-SM_APP_ID=$(bashio::config 'solarman_app_id' | jq -Rsa .)
-SM_SECRET=$(bashio::config 'solarman_secret' | jq -Rsa .)
+SM_APP_ID=$(bashio::config 'solarman_app_id')
+SM_SECRET=$(bashio::config 'solarman_secret')
 SM_NAME=$(bashio::config 'solarman_name' | jq -Rsa .)
-SM_STATION=$(bashio::config 'solarman_station'  | jq -Rsa .)
-SM_INVERTER=$(bashio::config 'solarman_inverter'  | jq -Rsa .)
-SM_LOGGER=$(bashio::config 'solarman_logger'  | jq -Rsa .)
+SM_STATION=$(bashio::config 'solarman_station')
+SM_INVERTER=$(bashio::config 'solarman_inverter')
+SM_LOGGER=$(bashio::config 'solarman_logger')
 
 bashio::log.info "Generate Solarman passhash from password"
 SM_HASH=$(exec python3 /app/solarman-mqtt/run.py --create-passhash "$SM_PASSWORD")
@@ -33,7 +33,7 @@ MQTT_USERNAME=$(bashio::services mqtt "username")
 MQTT_PASSWORD=$(bashio::services mqtt "password")
 
 bashio::log.info "Pull MQTT topic from add-on config"
-MQTT_TOPIC=$(bashio::config 'mqtt_topic'  | jq -Rsa .)
+MQTT_TOPIC=$(bashio::config 'mqtt_topic')
 
 
 bashio::log.info "Creating Solarman-MQTT configuration file"
@@ -41,20 +41,20 @@ cat << EOF > config.json
 {
   "name": $SM_NAME,
   "url": "globalapi.solarmanpv.com",
-  "appid": $SM_APP_ID,
-  "secret": $SM_SECRET,
-  "username": $SM_USERNAME,
+  "appid": "$SM_APP_ID",
+  "secret": "$SM_SECRET",
+  "username": "$SM_USERNAME",
   "passhash": "$SM_HASH",
   "stationId": $SM_STATION,
   "inverterId": $SM_INVERTER,
   "loggerId": $SM_LOGGER,
   "debug": false,
   "mqtt":{
-    "broker": $MQTT_BROKER,
+    "broker": "$MQTT_BROKER",
     "port": $MQTT_PORT,
-    "topic": $MQTT_TOPIC,
-    "username": $MQTT_USERNAME,
-    "password": $MQTT_PASSWORD,
+    "topic": "$MQTT_TOPIC",
+    "username": "$MQTT_USERNAME",
+    "password": "$MQTT_PASSWORD",
     "qos": 1,
     "retain": true
   }
