f2f21f925f134d897dd348d6864c4499e2d1a96c
Austin Mroczek <austin@mroczek.org>
2024-11-10 20:23:06 +0000

    Revert "fix other config options"
    This reverts commit f9b83fae29538de6f195eddb4cd2cfe99726da74.

--- solarman_mqtt/run.sh
+++ solarman_mqtt/run.sh
@@ -13,24 +13,24 @@ if [ ! -f "config.json" ]; then
 fi
 
 bashio::log.info "Pull Solarman config from HA add-on config"
-SM_USERNAME=$(bashio::config 'solarman_username' | jq -Rsa .)
+SM_USERNAME=$(bashio::config 'solarman_username'  | jq -Rsa .)
 SM_PASSWORD=$(bashio::config 'solarman_password')
 SM_APP_ID=$(bashio::config 'solarman_app_id' | jq -Rsa .)
 SM_SECRET=$(bashio::config 'solarman_secret' | jq -Rsa .)
 SM_NAME=$(bashio::config 'solarman_name' | jq -Rsa .)
-SM_STATION=$(bashio::config 'solarman_station' | jq -Rsa .)
-SM_INVERTER=$(bashio::config 'solarman_inverter' | jq -Rsa .)
-SM_LOGGER=$(bashio::config 'solarman_logger' | jq -Rsa .)
+SM_STATION=$(bashio::config 'solarman_station'  | jq -Rsa .)
+SM_INVERTER=$(bashio::config 'solarman_inverter'  | jq -Rsa .)
+SM_LOGGER=$(bashio::config 'solarman_logger'  | jq -Rsa .)
 
 bashio::log.info "Generate Solarman passhash from password"
 SM_HASH=$(exec python3 /app/solarman-mqtt/run.py --create-passhash "$SM_PASSWORD")
 bashio::log.info "Hash is $SM_HASH"
 
 bashio::log.info "Pull MQTT config from MQTT add-on"
-MQTT_BROKER=$(bashio::services mqtt "host" | jq -Rsa .)
-MQTT_PORT=$(bashio::services mqtt "port" | jq -Rsa .)
-MQTT_USERNAME=$(bashio::services mqtt "username" | jq -Rsa .)
-MQTT_PASSWORD=$(bashio::services mqtt "password" | jq -Rsa .)
+MQTT_BROKER=$(bashio::services mqtt "host")
+MQTT_PORT=$(bashio::services mqtt "port")
+MQTT_USERNAME=$(bashio::services mqtt "username")
+MQTT_PASSWORD=$(bashio::services mqtt "password")
 
 bashio::log.info "Pull MQTT topic from add-on config"
 MQTT_TOPIC=$(bashio::config 'mqtt_topic'  | jq -Rsa .)
